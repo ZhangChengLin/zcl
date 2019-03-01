7e81ffe263c00bf666b7b2b726db003346fb7a73
lin <469946668@qq.com>
2019-03-01 21:56:28 +0800

    初始

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,9 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
-
+//path
 const jquery_path = './node_modules/jquery/dist/jquery.min.js';
 const bootstrap_css_path = './node_modules/bootstrap/dist/css/bootstrap.min.css';
 const bootstrap_js_path = './node_modules/bootstrap/dist/js/bootstrap.min.js';
 const popper_path = './node_modules/bootstrap/dist/umd/popper.min.js';
+
