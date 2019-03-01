e81aa467321e0512ffbf3cfd181b12104179ea8b
lin <469946668@qq.com>
2019-03-01 23:15:52 +0800

    初始

--- gulpfile.js
+++ gulpfile.js
@@ -8,5 +8,21 @@ const rename = require('gulp-rename');
 const jquery_path = './node_modules/jquery/dist/jquery.min.js';
 const bootstrap_css_path = './node_modules/bootstrap/dist/css/bootstrap.min.css';
 const bootstrap_js_path = './node_modules/bootstrap/dist/js/bootstrap.min.js';
-const popper_path = './node_modules/bootstrap/dist/umd/popper.min.js';
+const popper_path = './node_modules/popper.js/dist/umd/popper.min.js';
+const animate_path = './node_modules/animate.css/animate.min.css';
 
+gulp.task('copy_static_js', copy_static_js);
+gulp.task('copy_static_css', copy_static_css);
+
+function copy_static_js(done) {
+    gulp.src([jquery_path]).pipe(gulp.dest('./assets/js/'));
+    gulp.src([bootstrap_js_path]).pipe(gulp.dest('./assets/js/'));
+    gulp.src([popper_path]).pipe(gulp.dest('./assets/js/'));
+    done();
+}
+
+function copy_static_css(done) {
+    gulp.src([bootstrap_css_path]).pipe(gulp.dest('./assets/css/'));
+    gulp.src([animate_path]).pipe(gulp.dest('./assets/css/'));
+    done();
+}
--- index.php
+++ index.php
@@ -7,9 +7,14 @@ global $zcl;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
     <title><?php echo $zcl['site_title'] ?></title>
 </head>
 <body>
+<div class="container text-center font-weight-bold">欢迎来到张成林的自留地</div>
 
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
 </body>
 </html>
