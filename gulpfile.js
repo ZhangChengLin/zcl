a5a94d9668afdeca95ca05b90547e9962c4bdd14
lin <469946668@qq.com>
2019-03-04 11:36:36 +0800

    初始

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,8 @@ const minifycss = require('gulp-uglifycss');
 const jquery_path = './node_modules/jquery/dist/jquery.min.js';
 const bootstrap_css_path = './node_modules/bootstrap/dist/css/bootstrap.min.css';
 const bootstrap_js_path = './node_modules/bootstrap/dist/js/bootstrap.min.js';
-const popper_path = './node_modules/popper.js/dist/umd/popper.min.js';
-const animate_path = './node_modules/animate.css/animate.min.css';
+const popper_js_path = './node_modules/popper.js/dist/umd/popper.min.js';
+const animate_css_path = './node_modules/animate.css/animate.min.css';
 const fontawesome_css_path = './node_modules/@fortawesome/fontawesome-free/css/*';
 const fontawesome_js_path = './node_modules/@fortawesome/fontawesome-free/js/*';
 const fontawesome_sprites_path = './node_modules/@fortawesome/fontawesome-free/sprites/*';
@@ -30,7 +30,6 @@ gulp.task('zcl_min_js', zcl_min_js);
 gulp.task('zcl_min_css', zcl_min_css);
 
 function zcl_min_js(done) {
-    'use strict';
     gulp.src([zcl_js_path])
         .pipe(terser())
         .pipe(rename('zcl.min.js'))
@@ -39,7 +38,6 @@ function zcl_min_js(done) {
 }
 
 function zcl_min_css(done) {
-    'use strict';
     gulp.src([zcl_css_path])
         .pipe(minifycss())
         .pipe(rename('zcl.min.css'))
@@ -48,15 +46,12 @@ function zcl_min_css(done) {
 }
 
 function copy_static_js(done) {
-    gulp.src([jquery_path]).pipe(gulp.dest('./assets/js/'));
-    gulp.src([bootstrap_js_path]).pipe(gulp.dest('./assets/js/'));
-    gulp.src([popper_path]).pipe(gulp.dest('./assets/js/'));
+    gulp.src([jquery_path, bootstrap_js_path, popper_js_path]).pipe(gulp.dest('./assets/js/'));
     done();
 }
 
 function copy_static_css(done) {
-    gulp.src([bootstrap_css_path]).pipe(gulp.dest('./assets/css/'));
-    gulp.src([animate_path]).pipe(gulp.dest('./assets/css/'));
+    gulp.src([bootstrap_css_path, animate_css_path]).pipe(gulp.dest('./assets/css/'));
     done();
 }
 
